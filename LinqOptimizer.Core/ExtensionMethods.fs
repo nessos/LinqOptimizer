9d924b084261b2654520523d36f024caf640c84e
npal <nick.palladinos@gmail.com>
2013-10-09 13:58:19 +0300

    Type sig refactorings

--- LinqOptimizer.Core/ExtensionMethods.fs
+++ LinqOptimizer.Core/ExtensionMethods.fs
@@ -16,7 +16,7 @@ namespace LinqOptimizer.Core
         // SequentialQuery ExtensionMethods
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member AsQueryExpr(enumerable : IEnumerable<'T>) = 
+        static member AsQueryExpr(enumerable : IEnumerable<'T>) : QueryExpr<IEnumerable<'T>> = 
             // Hack to optimize Enumerable.Range and Enumerable.Repeat calls
             // TODO : check Mono generated types
             let ty = enumerable.GetType()
@@ -24,12 +24,12 @@ namespace LinqOptimizer.Core
             | s when s.StartsWith "System.Linq.Enumerable+<RangeIterator>"  ->
                 let start = ty.GetFields().First(fun f -> f.Name.EndsWith "__start").GetValue(enumerable)
                 let count = ty.GetFields().First(fun f -> f.Name.EndsWith "__count").GetValue(enumerable)
-                new QueryExpr<IEnumerable<'T>>(RangeGenerator(start :?> int, count :?> int))
+                new QueryExpr<_>(RangeGenerator(start :?> int, count :?> int))
             | s when s.StartsWith "System.Linq.Enumerable+<RepeatIterator>"  ->
                 let element = ty.GetFields().First(fun f -> f.Name.EndsWith "__element").GetValue(enumerable)
                 let count = ty.GetFields().First(fun f -> f.Name.EndsWith "__count").GetValue(enumerable)
-                new QueryExpr<IEnumerable<'T>>(RepeatGenerator(element, typeof<'T>, count :?> int))
-            | _ -> new QueryExpr<IEnumerable<'T>>(Source (constant enumerable, typeof<'T>))
+                new QueryExpr<_>(RepeatGenerator(element, typeof<'T>, count :?> int))
+            | _ -> new QueryExpr<_>(Source (constant enumerable, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member Compile<'T>(queryExpr : QueryExpr<'T>) : Func<'T> =
@@ -52,35 +52,35 @@ namespace LinqOptimizer.Core
             ExtensionMethods.Compile(queryExpr).Invoke()
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Select<'T, 'R>(queryExpr : QueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, 'R>>) =
-            new QueryExpr<IEnumerable<'R>>(Transform (selector, queryExpr.QueryExpr, typeof<'R>))
+        static member Select<'T, 'R>(queryExpr : QueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, 'R>>) : QueryExpr<IEnumerable<'R>> =
+            new QueryExpr<_>(Transform (selector, queryExpr.QueryExpr, typeof<'R>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Select<'T, 'R>(queryExpr : QueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, int, 'R>>) =
-            new QueryExpr<IEnumerable<'R>>(TransformIndexed (selector, queryExpr.QueryExpr, typeof<'R>))
+        static member Select<'T, 'R>(queryExpr : QueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, int, 'R>>) : QueryExpr<IEnumerable<'R>> =
+            new QueryExpr<_>(TransformIndexed (selector, queryExpr.QueryExpr, typeof<'R>))
             
         [<System.Runtime.CompilerServices.Extension>]
-        static member Where<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, bool>>) =
-            new QueryExpr<IEnumerable<'T>>(Filter (predicate, queryExpr.QueryExpr, typeof<'T>))
+        static member Where<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, bool>>) : QueryExpr<IEnumerable<'T>> =
+            new QueryExpr<_>(Filter (predicate, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Where<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, int, bool>>) =
-            new QueryExpr<IEnumerable<'T>>(FilterIndexed (predicate, queryExpr.QueryExpr, typeof<'T>))
+        static member Where<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, int, bool>>) : QueryExpr<IEnumerable<'T>> =
+            new QueryExpr<_>(FilterIndexed (predicate, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Aggregate(queryExpr : QueryExpr<IEnumerable<'T>>, seed : 'Acc, func : Expression<Func<'Acc, 'T, 'Acc>>) =
+        static member Aggregate(queryExpr : QueryExpr<IEnumerable<'T>>, seed : 'Acc, func : Expression<Func<'Acc, 'T, 'Acc>>) : QueryExpr<'Acc> =
             new QueryExpr<'Acc>(Aggregate ((seed :> _, typeof<'Acc>), func, queryExpr.QueryExpr))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Sum(queryExpr : QueryExpr<IEnumerable<double>>) =
+        static member Sum(queryExpr : QueryExpr<IEnumerable<double>>) : QueryExpr<double> =
             new QueryExpr<double>(Sum (queryExpr.QueryExpr, typeof<double>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Sum(queryExpr : QueryExpr<IEnumerable<int>>) =
+        static member Sum(queryExpr : QueryExpr<IEnumerable<int>>) : QueryExpr<int> =
             new QueryExpr<int>(Sum (queryExpr.QueryExpr, typeof<int>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Count(queryExpr : QueryExpr<IEnumerable<'T>>) =
+        static member Count(queryExpr : QueryExpr<IEnumerable<'T>>) : QueryExpr<'T> =
             new QueryExpr<'T>(Count (queryExpr.QueryExpr, typeof<'T>))
             
         [<System.Runtime.CompilerServices.Extension>]
@@ -93,7 +93,7 @@ namespace LinqOptimizer.Core
                     NestedQueryTransform ((paramExpr, Compiler.toQueryExpr bodyExpr), resultSelector, queryExpr.QueryExpr, typeof<'R>)
                 | _ -> failwithf "Invalid state %A" collectionSelector
 
-            new QueryExpr<IEnumerable<'R>>(queryExpr')
+            new QueryExpr<_>(queryExpr')
 
         [<System.Runtime.CompilerServices.Extension>]
         static member SelectMany<'T, 'R>(queryExpr : QueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, IEnumerable<'R>>>) : QueryExpr<IEnumerable<'R>> =
@@ -103,16 +103,16 @@ namespace LinqOptimizer.Core
                     NestedQuery ((paramExpr, Compiler.toQueryExpr bodyExpr), queryExpr.QueryExpr, typeof<'R>)
                 | _ -> failwithf "Invalid state %A" selector
 
-            new QueryExpr<IEnumerable<'R>>(queryExpr')
+            new QueryExpr<_>(queryExpr')
 
 
         [<System.Runtime.CompilerServices.Extension>]
         static member Take<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, n : int) : QueryExpr<IEnumerable<'T>> =
-            new QueryExpr<IEnumerable<'T>>(Take (constant n, queryExpr.QueryExpr, typeof<'T>))
+            new QueryExpr<_>(Take (constant n, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member Skip<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, n : int) : QueryExpr<IEnumerable<'T>> =
-            new QueryExpr<IEnumerable<'T>>(Skip (constant n, queryExpr.QueryExpr, typeof<'T>))
+            new QueryExpr<_>(Skip (constant n, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member ForEach<'T>(queryExpr : QueryExpr<IEnumerable<'T>>, action : Expression<Action<'T>>) : QueryExprVoid =
@@ -121,30 +121,30 @@ namespace LinqOptimizer.Core
         [<System.Runtime.CompilerServices.Extension>]
         static member GroupBy<'T, 'Key>(queryExpr : QueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>)
                                 : QueryExpr<IEnumerable<IGrouping<'Key, 'T>>> = 
-            new QueryExpr<IEnumerable<IGrouping<'Key, 'T>>>(GroupBy (keySelector, queryExpr.QueryExpr, typeof<IGrouping<'Key, 'T>>))
+            new QueryExpr<_>(GroupBy (keySelector, queryExpr.QueryExpr, typeof<IGrouping<'Key, 'T>>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member OrderBy<'T, 'Key>(queryExpr : QueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>)
                                 : QueryExpr<IEnumerable<'T>> = 
-            new QueryExpr<IEnumerable<'T>>(OrderBy (keySelector, Order.Ascending, queryExpr.QueryExpr, typeof<'T>))
+            new QueryExpr<_>(OrderBy (keySelector, Order.Ascending, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member OrderByDescending<'T, 'Key>(queryExpr : QueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>)
                                 : QueryExpr<IEnumerable<'T>> = 
-            new QueryExpr<IEnumerable<'T>>(OrderBy (keySelector, Order.Descending, queryExpr.QueryExpr, typeof<'T>))
+            new QueryExpr<_>(OrderBy (keySelector, Order.Descending, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member ToList(queryExpr : QueryExpr<IEnumerable<'T>>) : QueryExpr<List<'T>> = 
-            new QueryExpr<List<'T>>(ToList queryExpr.QueryExpr)
+            new QueryExpr<_>(ToList queryExpr.QueryExpr)
 
         [<System.Runtime.CompilerServices.Extension>]
         static member ToArray(queryExpr : QueryExpr<IEnumerable<'T>>) : QueryExpr<'T []> = 
-            new QueryExpr<'T []>(ToArray queryExpr.QueryExpr)
+            new QueryExpr<_>(ToArray queryExpr.QueryExpr)
 
         // ParallelQuery ExtensionMethods
         [<System.Runtime.CompilerServices.Extension>]
-        static member AsQueryExpr(parallelQuery : ParallelQuery<'T>) = 
-            new ParallelQueryExpr<IEnumerable<'T>>(Source (constant parallelQuery, typeof<'T>))
+        static member AsQueryExpr(parallelQuery : ParallelQuery<'T>) : ParallelQueryExpr<IEnumerable<'T>> = 
+            new ParallelQueryExpr<_>(Source (constant parallelQuery, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
         static member Compile<'T>(queryExpr : ParallelQueryExpr<'T>) : Func<'T> =
@@ -157,51 +157,58 @@ namespace LinqOptimizer.Core
             ExtensionMethods.Compile(queryExpr).Invoke()
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Select<'T, 'R>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, 'R>>) =
-            new ParallelQueryExpr<IEnumerable<'R>>(Transform (selector, queryExpr.QueryExpr, typeof<'R>))
+        static member Select<'T, 'R>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, 'R>>)
+                        : ParallelQueryExpr<IEnumerable<'R>> =
+            new ParallelQueryExpr<_>(Transform (selector, queryExpr.QueryExpr, typeof<'R>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Where<'T>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, bool>>) =
-            new ParallelQueryExpr<IEnumerable<'T>>(Filter (predicate, queryExpr.QueryExpr, typeof<'T>))
+        static member Where<'T>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, predicate : Expression<Func<'T, bool>>) 
+                        : ParallelQueryExpr<IEnumerable<'T>> =
+            new ParallelQueryExpr<_>(Filter (predicate, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Sum(queryExpr : ParallelQueryExpr<IEnumerable<double>>) =
+        static member Sum(queryExpr : ParallelQueryExpr<IEnumerable<double>>) : ParallelQueryExpr<double> =
             new ParallelQueryExpr<double>(Sum (queryExpr.QueryExpr, typeof<double>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member Sum(queryExpr : ParallelQueryExpr<IEnumerable<int>>) =
+        static member Sum(queryExpr : ParallelQueryExpr<IEnumerable<int>>) : ParallelQueryExpr<int> =
             new ParallelQueryExpr<int>(Sum (queryExpr.QueryExpr, typeof<int>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member SelectMany<'T, 'R>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, IEnumerable<'R>>>) =
+        static member SelectMany<'T, 'R>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, selector : Expression<Func<'T, IEnumerable<'R>>>) 
+                        : ParallelQueryExpr<IEnumerable<'R>> =
             let queryExpr' = 
                 match selector with
                 | Lambda ([paramExpr], bodyExpr) ->
                     NestedQuery ((paramExpr, Compiler.toQueryExpr bodyExpr), queryExpr.QueryExpr, typeof<'R>)
                 | _ -> failwithf "Invalid state %A" selector
 
-            new ParallelQueryExpr<IEnumerable<'R>>(queryExpr')
+            new ParallelQueryExpr<_>(queryExpr')
 
         [<System.Runtime.CompilerServices.Extension>]
         static member SelectMany<'T, 'Col, 'R>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, 
                                                 collectionSelector : Expression<Func<'T, IEnumerable<'Col>>>, 
-                                                resultSelector : Expression<Func<'T, 'Col, 'R>>) =
+                                                resultSelector : Expression<Func<'T, 'Col, 'R>>) 
+                            : ParallelQueryExpr<IEnumerable<'R>> =
             let queryExpr' = 
                 match collectionSelector with
                 | Lambda ([paramExpr], bodyExpr) ->
                     NestedQueryTransform ((paramExpr, Compiler.toQueryExpr bodyExpr), resultSelector, queryExpr.QueryExpr, typeof<'R>)
                 | _ -> failwithf "Invalid state %A" collectionSelector
 
-            new ParallelQueryExpr<IEnumerable<'R>>(queryExpr')
+            new ParallelQueryExpr<_>(queryExpr')
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member GroupBy<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) =
-            new ParallelQueryExpr<IEnumerable<IGrouping<'Key, 'T>>>(GroupBy (keySelector, queryExpr.QueryExpr, typeof<IGrouping<'Key, 'T>>))
+        static member GroupBy<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) 
+                        : ParallelQueryExpr<IEnumerable<IGrouping<'Key, 'T>>> =
+            new ParallelQueryExpr<_>(GroupBy (keySelector, queryExpr.QueryExpr, typeof<IGrouping<'Key, 'T>>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member OrderBy<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) = 
-            new ParallelQueryExpr<IEnumerable<'T>>(OrderBy (keySelector, Order.Ascending, queryExpr.QueryExpr, typeof<'T>))
+        static member OrderBy<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) 
+                                    : ParallelQueryExpr<IEnumerable<'T>> = 
+            new ParallelQueryExpr<_>(OrderBy (keySelector, Order.Ascending, queryExpr.QueryExpr, typeof<'T>))
 
         [<System.Runtime.CompilerServices.Extension>]
-        static member OrderByDescending<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) = 
-            new ParallelQueryExpr<IEnumerable<'T>>(OrderBy (keySelector, Order.Descending, queryExpr.QueryExpr, typeof<'T>))
+        static member OrderByDescending<'T, 'Key>(queryExpr : ParallelQueryExpr<IEnumerable<'T>>, keySelector : Expression<Func<'T, 'Key>>) 
+                                : ParallelQueryExpr<IEnumerable<'T>> = 
+            new ParallelQueryExpr<_>(OrderBy (keySelector, Order.Descending, queryExpr.QueryExpr, typeof<'T>))
