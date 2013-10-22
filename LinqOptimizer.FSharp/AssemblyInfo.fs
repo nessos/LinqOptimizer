af16d57be5353f2e5a529ffe3bcf612f8fc5fe9a
krontogiannis <rodoconst@hotmail.com>
2013-10-22 19:32:25 +0300

    Added AssemblyInfo

---
+++ LinqOptimizer.FSharp/AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿
+namespace LinqOptimizer.FSharp
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+ 
+[<assembly: AssemblyVersion(LinqOptimizer.Core.Version.Number)>]
+do ()
