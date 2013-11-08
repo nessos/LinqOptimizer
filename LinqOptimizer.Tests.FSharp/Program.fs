470d6d57410e2a72d07017568dbf7e23a17baffb
krontogiannis <rodoconst@hotmail.com>
2013-11-08 19:14:33 +0200

    F# Expression optimizer implemented.

--- LinqOptimizer.Tests.FSharp/Program.fs
+++ LinqOptimizer.Tests.FSharp/Program.fs
@@ -10,7 +10,7 @@ module Program =
     let main argv = 
         
         let test = new ``F# Query tests``()
-        let t = test.map()
+        let t = test.filter()
 
         
 //        let xs = 
