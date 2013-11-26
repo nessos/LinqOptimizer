ad80350cc3d7e2fcc9c8858c327f7c0bb3c53ea0
krontogiannis <rodoconst@hotmail.com>
2013-11-26 16:59:02 +0200

    Added benchmarks projects

---
+++ LinqOptimizer.Benchmarks.CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqOptimizer.Benchmarks.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
