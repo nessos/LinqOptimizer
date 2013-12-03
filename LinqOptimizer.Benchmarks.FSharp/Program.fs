e8a928df25613b6eb9978ed216fe7f2cdd21ead6
krontogiannis <rodoconst@hotmail.com>
2013-12-03 17:45:48 +0200

    added F# benchmarks

--- LinqOptimizer.Benchmarks.FSharp/Program.fs
+++ LinqOptimizer.Benchmarks.FSharp/Program.fs
@@ -1,9 +1,10 @@
 ﻿open System
+open System.Linq
 open System.Diagnostics
 open LinqOptimizer.FSharp
 open LinqOptimizer.Base
 
-let measure<'T>(title1, action1, title2, action2, validate) =
+let measuref<'T>(title1, action1 : unit -> 'T, title2, action2 : unit -> 'T, validate : 'T * 'T -> bool) =
     let sw = new Stopwatch();
     sw.Start();
     let t1 = action1();
@@ -25,63 +26,71 @@ let SumLinqOpt(values : double[]) =
 let SumSqLinq(values : double[]) =
     values |> Seq.map (fun x -> x * x) |> Seq.sum
 
-//let SumSqLinqOpt(values : double[])
-//    return values.AsQueryExpr().Select(x => x * x).Sum().Run();
+let SumSqLinqOpt(values : double[]) =
+    values |> Query.ofSeq |> Query.map(fun x -> x * x) |> Query.sum |> Query.run
 
-//static double CartLinq(double[] dim1, double[] dim2)
-//{
-//    return (from x in dim1
-//            from y in dim2
-//            select x * y).Sum();
-//}
-//
-//static double CartLinqOpt(double[] dim1, double[] dim2)
-//{
-//    return (from x in dim1.AsQueryExpr()
-//            from y in dim2
-//            select x * y).Sum().Run();
-//}
-//
-//static int[] GroupLinq(int size)
-//{
-//    var rnd = new Random(size);
-//    return Enumerable.Range(1, size)
-//            .Select(x => 100 * rnd.NextDouble() - 50)
-//            .GroupBy(x => (int)x % 10)
-//            .Select(x => x.Count())
-//            .ToArray();
-//}
-//
-//static int[] GroupLinqOpt(int size)
-//{
-//    var rnd = new Random(size);
-//    return Enumerable.Range(1, size).AsQueryExpr()
-//            .Select(x => 100 * rnd.NextDouble() - 50)
-//            .GroupBy(x => (int)x % 10)
-//            .Select(x => x.Count())
-//            .ToArray()
-//            .Run();
-//}
-//
-//static Tuple<int, int, int>[] PythagoreanTriplesLinq(int max)
-//{
-//    return (from a in Enumerable.Range(1, max + 1)
-//            from b in Enumerable.Range(a, max + 1 - a)
-//            from c in Enumerable.Range(b, max + 1 - b)
-//            where a * a + b * b == c * c
-//            select Tuple.Create(a, b, c)).ToArray();
-//}
-//
-//static Tuple<int, int, int>[] PythagoreanTriplesLinqOpt(int max)
-//{
-//    return (from a in Enumerable.Range(1, max + 1).AsQueryExpr()
-//            from b in Enumerable.Range(a, max + 1 - a)
-//            from c in Enumerable.Range(b, max + 1 - b)
-//            where a * a + b * b == c * c
-//            select Tuple.Create(a, b, c)).ToArray().Run();
-//}
+let CartLinq (dim1 : double[], dim2 : double[]) =
+    dim1 |> Seq.collect (fun x -> Seq.map (fun y -> x * y) dim2) |> Seq.sum
+
+let CartLinqOpt(dim1 : double[], dim2 : double[]) =
+    dim1 |> Query.ofSeq |> Query.collect (fun x -> Seq.map (fun y -> x * y) dim2) |> Query.sum |> Query.run
+
+
+let GroupLinq(size) =
+    let rnd = new Random(size);
+    Enumerable.Range(1, size)
+    |> Seq.map (fun x -> 100. * rnd.NextDouble() - 50.)
+    |> Seq.groupBy(fun x -> int x % 10)
+    |> Seq.map(fun (_,x) -> Seq.length x)
+    |> Seq.toArray
+
+let GroupLinqOpt(size) =
+    let rnd = new Random(size);
+    Enumerable.Range(1, size)
+    |> Query.ofSeq
+    |> Query.map (fun x -> 100. * rnd.NextDouble() - 50.)
+    |> Query.groupBy(fun x -> int x % 10)
+    |> Query.map(fun (_,x) -> Seq.length x)
+    |> Query.toArray
+    |> Query.run
+
+
+let PythagoreanTriplesLinq(max) =
+    Enumerable.Range(1, max + 1)
+    |> Seq.collect(fun a ->
+        Enumerable.Range(a, max + 1 - a)
+        |> Seq.collect(fun b ->
+            Enumerable.Range(b, max + 1 - b)
+            |> Seq.map (fun c -> a, b, c)))
+    |> Seq.filter (fun (a,b,c) -> a * a + b * b = c * c)
+    |> Seq.toArray
+                
+
+let PythagoreanTriplesLinqOpt(max) =
+    Enumerable.Range(1, max + 1)
+    |> Query.ofSeq
+    |> Query.collect(fun a ->
+        Enumerable.Range(a, max + 1 - a)
+        |> Seq.collect(fun b ->
+            Enumerable.Range(b, max + 1 - b)
+            |> Seq.map (fun c -> a, b, c)))
+    |> Query.filter (fun (a,b,c) -> a * a + b * b = c * c)
+    |> Query.toArray
+    |> Query.run
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let vs = Array.init 50000000 double
+
+    measuref("Sum Linq", (fun () -> SumLinq vs), "Sum Opt", (fun () -> SumLinqOpt vs), fun (x, y) -> x = y)
+    
+    measuref("Sum Sq Linq", (fun () -> SumSqLinq vs), "Sum Sq Opt", (fun () -> SumSqLinqOpt vs), fun (x, y) -> x = y)
+    
+    let s = 50000000
+    measuref("Group Linq", (fun () -> GroupLinq s), "Group Opt", (fun () -> GroupLinqOpt s), fun (x, y) -> x = y)
+    
+    //``measure``("Sum Linq", (fun () -> SumLinq vs), "Sum Opt", (fun () -> SumLinqOpt vs), fun (x, y) -> x = y)
+
+
+
     0 // return an integer exit code
