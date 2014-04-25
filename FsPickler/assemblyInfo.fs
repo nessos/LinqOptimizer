1da03b93ffeb9c8e030d6ace7604aa6043d77633
eiriktsarpalis <eirik.tsarpalis@gmail.com>
2014-04-25 18:17:35 +0300

    cleanup

--- FsPickler/assemblyInfo.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace System
-open System.Reflection
-
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-do ()
-
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
